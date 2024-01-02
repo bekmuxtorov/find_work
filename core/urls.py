2b592adbedbd856f49971ccda7db87b9f5a35511
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-02 16:04:55 +0500

    🛠️ Bug fixed

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
+from django.conf.urls.i18n import i18n_patterns
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('rosetta/', include('rosetta.urls')),
 
-    path('api/', include('account.urls'))
-]
+    path('api/', include('account.urls')),
+)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
