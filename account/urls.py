384bd34c5d1b8341b7fcaf2e796259ba6ed33a10
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-03 15:11:32 +0500

    ✏️ Written Region and District list and detail api

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,13 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
+    # Region
+    path('regions/', views.RegionListAPIView.as_view()),
+    path('regions/<int:pk>', views.RegionDetailAPIView.as_view()),
+
+    # District
+    path('districts/', views.DistrictListAPIView.as_view()),
+    path('districts/<int:pk>', views.DistrictDetailAPIView.as_view()),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from . import serializers
+from . import models
+
+
+# Region List API View
+class RegionListAPIView(generics.ListAPIView):
+    queryset = models.Region.objects.all()
+    serializer_class = serializers.RegionSerializer
+
+
+# Region Detail API View
+class RegionDetailAPIView(generics.RetrieveAPIView):
+    queryset = models.Region.objects.all()
+    serializer_class = serializers.RegionSerializer
+
+
+# District List API View
+class DistrictListAPIView(generics.ListAPIView):
+    queryset = models.District.objects.all()
+    serializer_class = serializers.DistrictSerializer
+
+
+# District Detail API View
+class DistrictDetailAPIView(generics.RetrieveAPIView):
+    queryset = models.District.objects.all()
+    serializer_class = serializers.DistrictSerializer
