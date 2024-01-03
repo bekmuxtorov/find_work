6d4adb049c1d9532cb464e82c60f47860760499f
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-03 20:49:31 +0500

    📝 Added register and login path

--- account/urls.py
+++ account/urls.py
@@ -5,9 +5,15 @@ from . import views
 urlpatterns = [
     # Region
     path('regions/', views.RegionListAPIView.as_view()),
-    path('regions/<int:pk>', views.RegionDetailAPIView.as_view()),
+    path('regions/<int:pk>/', views.RegionDetailAPIView.as_view()),
 
     # District
     path('districts/', views.DistrictListAPIView.as_view()),
-    path('districts/<int:pk>', views.DistrictDetailAPIView.as_view()),
+    path('districts/<int:pk>/', views.DistrictDetailAPIView.as_view()),
+
+    # User Registration
+    path('auth/register/', views.UserRegistrationAPIView.as_view()),
+
+    # User Login
+    path('auth/login/', views.UserLoginAPIView.as_view())
 ]
