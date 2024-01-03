c4332adbac95769a4f202941f198429e571e48c9
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-03 14:45:52 +0500

    ➕ Added home page

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
 from .schema import swagger_urlpatterns
+
+
+def show_home(request):
+    return render(request, 'index.html')
+
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('rosetta/', include('rosetta.urls')),
 
+    path('', show_home),
     path('api/', include('account.urls')),
 )
 
