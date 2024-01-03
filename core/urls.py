3bac55ce8b998a210b8bcbfb8fbce9cb96c4522e
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-03 14:32:41 +0500

    📤 Added schema_urlspatters to urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.i18n import i18n_patterns
-from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .schema import swagger_urlpatterns
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
@@ -11,6 +11,8 @@ urlpatterns = i18n_patterns(
 
     path('api/', include('account.urls')),
 )
+
+urlpatterns += swagger_urlpatterns
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
