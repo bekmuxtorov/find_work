8ef18f5ef53fa9d1025100e6ce67f0a2db32570f
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-04 18:33:22 +0500

    📝 Added ChangePasswordSerializer

--- account/serializers.py
+++ account/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import check_password
 from rest_framework import serializers
 from . import models
 
@@ -14,6 +15,36 @@ class DistrictSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.District
         fields = '__all__'
+
+
+# Password alashmashtirish
+class ChangePasswordSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(required=True, max_length=20)
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def create(self, validated_data):
+        phone_number = validated_data.get("phone_number")
+        old_password = validated_data.get("old_password")
+        new_password = validated_data.get("new_password")
+
+        user = models.User.objects.filter(phone_number=phone_number).first()
+        if not user:
+            raise serializers.ValidationError(
+                {
+                    "error": "User not found"
+                }
+            )
+        if not user.check_password(old_password):
+            raise serializers.ValidationError(
+                {
+                    "error": "The old password entered with the current password did not match"
+                }
+            )
+
+        user.set_password(new_password)
+        user.save()
+        return user
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -51,5 +82,3 @@ class RegisterSerializer(serializers.ModelSerializer):
                     "error": "Both passwords do not match"
                 }
             )
-        
-    
